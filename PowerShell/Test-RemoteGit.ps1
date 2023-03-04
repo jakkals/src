333cf61927a581d1ee2bca5d3f211619da686f2d
Jacques Heydenrych <jakkalas@gmail.com>
2023-03-04 14:30:10 -0600

    Updated script with better instructions

--- PowerShell/Test-RemoteGit.ps1
+++ PowerShell/Test-RemoteGit.ps1
@@ -1,14 +1,18 @@
 ##################################################
 # Author: Jacques Heydenrych
 # Date: March 4, 2023
-# Test running a script from GitHub.
+# Test downloading and then running a script from GitHub.
 ##################################################
 # You may have to set the execution policy to unrestricted. First obtain your existing execution policy:
 #   Get-ExecutionPolicy
 # and make a note of what it currently is. Then change the execution policy:
 #   Set-ExecutionPolicy unrestricted
+# Obtain the script with:
+#   Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/jakkals/src/main/PowerShell/Test-RemoteGit.ps1' -OutFile '.\Test-RemoteGit.ps1'
 # Run the script with:
-#   Invoke-WebRequest-Uri 'http://jakkals.github.com/src/Powershell/Test-RemoteGit.ps1'
+#    ./Test-RemoteGit.ps1
+# Because this script s on the really useful, and it is probably stored in an awkward location:
+#    del .\Test-RemoteGit.ps1
 # Now change the execution policy back to what it had been. For example:
 #   Set-ExecutionPolicy remotesigned
 ###################################################
